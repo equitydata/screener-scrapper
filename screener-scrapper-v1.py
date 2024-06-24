adf99b9ecf5c385979bb0faee6346e6c16485fe0
equitydata <sangram.dash.equitydata@gmail.com>
2024-06-24 23:58:17 +0530

    screener-scrapper-v1.py

--- screener-scrapper-v1.py
+++ screener-scrapper-v1.py
@@ -3,7 +3,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 # URL of the Screener.in page
-screener_url = "https://www.screener.in/screens/1645194/equitydata-filtercofee/"
+screener_url = "https://www.screener.in/screens/1645194/equitydata-filter-coffee/"
 
 # Send an HTTP request to the URL
 response = requests.get(screener_url)
@@ -30,9 +30,9 @@ if response.status_code == 200:
         df = pd.DataFrame(rows, columns=headers)
 
         # Save the DataFrame to a CSV file
-        df.to_csv("/tmp/equitydata-filter-cofee.csv", index=False)
+        df.to_csv("/tmp/equitydata-filter-coffee.csv", index=False)
 
-        print("Table data saved to /tmp/equitydata-filter-cofee.csv ")
+        print("Table data saved to /tmp/equitydata-filter-coffee.csv ")
     else:
         print("Table not found.")
 else:
